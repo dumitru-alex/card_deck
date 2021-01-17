c47416d6153a48e85846bfd6ef32d5f20fb635c5
Alex Dumitru <dumitru.alex0@gmail.com>
2021-01-17 18:26:16 +0100

    add tests for PlayingDeck

---
+++ src/PlayingDeck.test.ts
@@ -0,0 +1,36 @@
+import { PlayingCard } from "./PlayingCard";
+import { PlayingDeck } from "./PlayingDeck";
+
+test("should create a 52 card deck", () => {
+  expect(new PlayingDeck().cards.length).toBe(52);
+});
+
+test("should shuffle the deck", () => {
+  const testDeck = new PlayingDeck();
+  const shuffledDeck = new PlayingDeck();
+  shuffledDeck.shuffle();
+  expect(testDeck.toString()).not.toBe(shuffledDeck.toString());
+});
+
+test("should deal 52 cards", () => {
+  const testDeck = new PlayingDeck();
+  let cardsDealt: PlayingCard[] = [];
+
+  for (let i = 0; i < testDeck.cards.length; i++) {
+    const card = testDeck.dealOneCard();
+    if (card) cardsDealt.push(card);
+  }
+
+  expect(cardsDealt.length).toBe(testDeck.cards.length);
+});
+
+test("should return undefined after dealing 52 cards", () => {
+  const testDeck = new PlayingDeck();
+
+  for (let i = 0; i < testDeck.cards.length; i++) {
+    testDeck.dealOneCard();
+  }
+  const fiftyThirdCard = testDeck.dealOneCard();
+
+  expect(fiftyThirdCard).toBe(undefined);
+});
