f106b00d9df2dbcc3da72674ae13144c634ebbb2
Alex Dumitru <dumitru.alex0@gmail.com>
2021-01-18 00:00:03 +0100

    change shuffling algorithm

--- src/PlayingDeck.ts
+++ src/PlayingDeck.ts
@@ -19,25 +19,19 @@ export class PlayingDeck implements Deck {
   get cards(): PlayingCard[] {
     return this.#cards;
   }
-  // Define a new playing card type, for Joker
   shuffle(): void {
-    // how is shuffling considerred ?
-    const nrOfShuffles = Math.floor(Math.random() * this.DECK_SIZE) + 1; // 0 <= 52 x Math.random() <= 52. Adding 1 to make sure we shuffle at least once
-    for (let i = nrOfShuffles; i > 0; i--) {
-      const randomFirstCard = this.#cards[
-        Math.floor(Math.random() * this.DECK_SIZE)
-      ]!;
-      const randomSecondCard = this.#cards[
-        Math.floor(Math.random() * this.DECK_SIZE)
-      ]!;
-      const temp = randomFirstCard;
-      this.#cards[this.#cards.indexOf(randomFirstCard)] = randomSecondCard;
-      this.#cards[this.#cards.indexOf(randomSecondCard)] = temp;
+    //  Fisher-Yates (Richard Durstenfeld version). See: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    for (let i = this.#cards.length - 1; i > 0; i--) {
+      console.log(i);
+      const j: number = Math.floor(Math.random() * (i + 1));
+      [this.#cards[i], this.#cards[j]] = [this.#cards[j]!, this.#cards[i]!];
     }
   }
+
   dealOneCard(): PlayingCard | undefined {
     return this.#cards.pop();
   }
+
   toString(): string {
     return this.#cards.reduce((prev, cur) => {
       const addNewLine = (prev: string) => {
@@ -47,3 +41,5 @@ export class PlayingDeck implements Deck {
     }, "");
   }
 }
+
+// Define a new playing card type, for Joker
